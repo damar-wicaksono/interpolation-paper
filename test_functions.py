23dfbd79d71b8b65345f5a3f37ed1a470bf779ea
Damar Wicaksono <d.wicaksono@hzdr.de>
2023-09-01 18:30:25 +0200

    Fix missing pi in the trig. test functions.

--- test_functions.py
+++ test_functions.py
@@ -20,14 +20,14 @@ def multidim_sin(xx: np.ndarray, parameter: int = 1) -> np.ndarray:
     """Compute the M-dimensional sine function."""
     params = parameter * np.ones(xx.shape[1])
 
-    return np.sin(np.sum(params * xx, axis=1))
+    return np.sin(np.pi * np.sum(params * xx, axis=1))
 
 
 def multidim_cos(xx: np.ndarray, parameter: int = 1) -> np.ndarray:
     """Compute the M-dimensional cosine function."""
     params = parameter * np.ones(xx.shape[1])
 
-    return np.cos(np.sum(params * xx, axis=1))
+    return np.cos(np.pi * np.sum(params * xx, axis=1))
 
 
 def multidim_cos_sin(xx: np.ndarray, parameter: list = [1, 1]) -> np.ndarray:
@@ -38,4 +38,4 @@ def multidim_cos_sin(xx: np.ndarray, parameter: list = [1, 1]) -> np.ndarray:
     xx_sin = params_1 * xx
     xx_cos = params_2 * xx
 
-    return np.cos(np.sum(xx_sin, axis=1)) + np.sin(np.sum(xx_cos, axis=1))
+    return np.cos(np.pi * np.sum(xx_sin, axis=1)) + np.sin(np.pi * np.sum(xx_cos, axis=1))
